2b4b1c00439a9361c7bcbf766947b34bcc36d619
EfremL <efremlehman@icloud.com>
2025-10-25 15:09:13 -0400

    Finished calc_load?

--- calc_load.py
+++ calc_load.py
@@ -1,17 +1,17 @@
 import pypsa
 import numpy as np
 
-def calc_total_load_gen(csv_name):
+def get_scale(csv_name):
     
     network = pypsa.Network()
     network.import_from_csv_folder(csv_name)
     info = network.pf()
     
     pload_total = network.loads['p_set'].sum().round(0)
-    print(f"Total load in network {pload_total}")
+    #print(f"Total load in network {pload_total}")
     
     pgen_total = network.generators['p_set'].sum().round(0)
-    print(f"Total load in network {pgen_total}")
+    #print(f"Total load in network {pgen_total}")
     
     x = np.arange(24)
     f = 1/24
@@ -20,13 +20,25 @@ def calc_total_load_gen(csv_name):
     C = 2*np.pi/2
     y = A * np.sin(2*np.pi * f * x + C) + offset
     
-    print(y.round(3).tolist())
+    return y
     
-def calc_load_gen_by_hour(csv_name, line_name):
+def calc_load(csv_name, line_name, hour):
     network = pypsa.Network()
     network.import_from_csv_folder(csv_name)
     info = network.pf() 
     
+    line_index = int(line_name[1]) 
     load_array = network.loads['p_set']
-    load = load_array[int(line_name[1])]
-    print(f"Load on {line_name}: {load}")
+    load = load_array[line_index]
+    #print(f"Load on {line_name}: {load}")
+    
+    scale = get_scale(csv_name)[hour]
+    #print(f"Scaling load by {scale}")
+    #print(load * scale)
+    
+    return load * scale
+    
+    
+
+    
+    
