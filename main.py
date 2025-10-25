92d024d21e3f359acc5e160f7fe7e1b5430a677f
FuchanKay <kazeuma0923@gmail.com>
2025-10-25 14:55:20 -0400

    Co-authored-by: XD1939 <XD1939@users.noreply.github.com> Co-authored-by: hooftie <hooftie@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # calc_load.calc_load_and_gen(path_name)
 # print("end")
 import pypsa
+import requests
 
 network = pypsa.Network()
 network.import_from_csv_folder("example_csv")
@@ -12,3 +13,6 @@ info = network.pf()
     
 pload_total = network.loads['p_set'].sum().round(0)
 print(f"Total load in network {pload_total}")
+
+api_url = ""
+
